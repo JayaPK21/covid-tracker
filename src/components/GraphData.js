74589a848e13622738153c56dfc757f7e5e6b9c2
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2024-01-15 11:00:33 +0000

    updates the title for graph component

--- src/components/GraphData.js
+++ src/components/GraphData.js
@@ -1,10 +1,15 @@
 import React from "react";
 import LineChart from "./LineChart";
 
+import { useSelector } from 'react-redux';
+
 function GraphData() {
+    //useSelectors for accessing data from the redux store
+    const datainfo = useSelector(state => state.datainfo);
+
     return(
         <div className="p-2 px-5">
-            <h5 className=" text-white">The following graph represents daily cases of Covid-19 over a one month period.</h5>
+            <h5 className=" text-white">The following graph represents daily cases of Covid-19 over a one month period for {datainfo.data[0].name} region.</h5>
             <LineChart />
         </div>
     );
